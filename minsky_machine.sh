ad2d849648980f4f10a8a562817e349a95792687
evgenii mikhailishin <djpon94@gmail.com>
2020-12-13 21:19:45 +0300

    add .sh script

---
+++ minsky_machine.sh
@@ -0,0 +1,3 @@
+export PATH="/home/evgeniy/.nvm/versions/node/v13.6.0/bin:/home/evgeniy/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/bin:/usr/lib/jvm/default/bin:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl"
+java --module-path ./javafx-sdk-11.0.2/lib/ --add-modules javafx.controls,javafx.fxml,javafx.graphics,javafx.web -jar out/artifacts/minsky_machine_jar/minsky_machine.jar 
+ 
